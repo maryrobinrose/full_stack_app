4e5d8e6c07e87ee999affd509466e405d0262993
Mary Robin Rose <maryrobinrose@gmail.com>
2019-06-07 16:09:16 -0700

    rearrangeg brackets

--- client/src/App.js
+++ client/src/App.js
@@ -9,25 +9,18 @@ import axios from 'axios';
 class App extends Component () {
   constructor() {
     super();
-    return (
     this.state = {
         courses: []
     };
-    componentDidMount() {
-      axios.get('http://localhost:5000/api/')
-      .then (res => res.json())
-      .then((data) => {
-        this.setState({ courses: data })
-      })
-      .catch(console.log)
-    }
-    /*<BrowserRouter>
-        <div className="container">
+  }
 
-        </div>
-      </BrowserRouter>*/
-
-    );
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/')
+    .then (res => res.json())
+    .then((data) => {
+      this.setState({ courses: data })
+    })
+    .catch(console.log)
   }
 }
 
