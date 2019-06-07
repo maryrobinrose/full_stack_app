0b09bfc3ddac8ce58d7a79dfc17c53283a880645
Mary Robin Rose <maryrobinrose@gmail.com>
2019-06-07 16:46:59 -0700

    remove parens from Component

--- client/src/App.js
+++ client/src/App.js
@@ -6,22 +6,31 @@ import {
 } from 'react-router-dom';
 import axios from 'axios';
 
-class App extends Component () {
-  constructor() {
-    super();
-    this.state = {
-        courses: []
+class App extends Component {
+    state = {
+          courses: []
     };
+
+
+    componentDidMount() {
+      axios.get('http://localhost:5000/api/')
+      .then (res => res.json())
+      .then((data) => {
+        this.setState({ courses: data })
+      })
+      .catch(console.log)
+    }
+
+    render() {
+      return (
+        <BrowserRouter>
+          <div>
+          </div>
+        </BrowserRouter>
+      );
+    }
+
   }
 
-  componentDidMount() {
-    axios.get('http://localhost:5000/api/')
-    .then (res => res.json())
-    .then((data) => {
-      this.setState({ courses: data })
-    })
-    .catch(console.log)
-  }
-}
 
 export default App;
