f88a738e148e3a3e9d721867f33feb7ce4e62017
Mary Robin Rose <maryrobinrose@gmail.com>
2019-06-07 16:04:49 -0700

    delete duplicate

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import {
   BrowserRouter,
   Route
 } from 'react-router-dom';
+import axios from 'axios';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div className="container">
+class App extends Component () {
+  constructor() {
+    super();
+    return (
+    this.state = {
+        courses: []
+    };
+    componentDidMount() {
+      axios.get('http://localhost:5000/api/')
+      .then (res => res.json())
+      .then((data) => {
+        this.setState({ courses: data })
+      })
+      .catch(console.log)
+    }
+    /*<BrowserRouter>
+        <div className="container">
 
-      </div>
-    </BrowserRouter>
-  );
+        </div>
+      </BrowserRouter>*/
+
+    );
+  }
 }
 
 export default App;
