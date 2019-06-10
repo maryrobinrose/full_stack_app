05cb0470fcecbdee51ab8faa3235a041a97b49de
Mary Robin Rose <maryrobinrose@gmail.com>
2019-06-09 18:50:31 -0700

    import components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
-import Courses from './components/Courses';
-//import './App.css';
 import {
   BrowserRouter,
   Route,
   Switch
 } from 'react-router-dom';
+
+
+//Import Components
+import CourseDetail from './components/CourseDetail';
+import Courses from './components/Courses';
+import CreateCourse from './components/CreatCourse';
+import Header from './components/Header';
+import UpdateCourse from './components/UpdateCourse';
+import UserSignIn from './components/UserSignIn';
+import UserSignOut from './components/UserSignOut';
+import UserSignUp from './components/UserSignUp';
 
 class App extends Component {
   render() {
