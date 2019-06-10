80e88901bda4016e0bdc178ebfdcc209435cab3e
Mary Robin Rose <maryrobinrose@gmail.com>
2019-06-09 17:55:52 -0700

    arrays showing in console

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -11,26 +11,12 @@ class Courses extends Component {
 
     componentDidMount() {
       //Request the API
-      axios.get('http://localhost:5000/api/courses')
+      axios.get(`http://localhost:5000/api/courses`)
       //Parse output to JSON
       .then (response => {
-        // create an array of contacts only with relevant data
-        const Courses = response.data.map(c => {
-          return {
-            title: c.title,
-            description: c.description
-          };
+        this.setState({
+          courses: response.data
         });
-
-        // create a new "State" object without mutating
-        // the original State object.
-        const newState = Object.assign({}, this.state, {
-          courses: Courses
-        });
-
-        // store the new state object in the component's state
-        this.setState(newState);
-
       })
       //Log errors
       .catch(error => {
@@ -38,14 +24,14 @@ class Courses extends Component {
       });
     }
 
-
     render() {
       console.log(this.state.courses);
-      return (
-        <div>
-          <h1>Course List</h1>
-        </div>
-      );
+        return (
+          <div>
+            <h1>Course List</h1>
+            <h2></h2>
+          </div>
+        )
     }
   }
 
