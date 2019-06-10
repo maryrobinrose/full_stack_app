6a49971444ea2f545c35f87c790a11895ee2ab6a
Mary Robin Rose <maryrobinrose@gmail.com>
2019-06-09 17:40:39 -0700

    working on rendering

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -14,7 +14,23 @@ class Courses extends Component {
       axios.get('http://localhost:5000/api/courses')
       //Parse output to JSON
       .then (response => {
-        this.setState({courses: response.data});
+        // create an array of contacts only with relevant data
+        const Courses = response.data.map(c => {
+          return {
+            title: c.title,
+            description: c.description
+          };
+        });
+
+        // create a new "State" object without mutating
+        // the original State object.
+        const newState = Object.assign({}, this.state, {
+          courses: Courses
+        });
+
+        // store the new state object in the component's state
+        this.setState(newState);
+
       })
       //Log errors
       .catch(error => {
@@ -24,17 +40,14 @@ class Courses extends Component {
 
 
     render() {
-      const { courses } = this.state;
+      console.log(this.state.courses);
       return (
-          <div>
-            <h1>Course List</h1>
-            {Object.keys(courses).map(key => {
-
-            })}
-          </div>
+        <div>
+          <h1>Course List</h1>
+        </div>
       );
     }
-
   }
 
-    export default Courses
+
+  export default Courses;
