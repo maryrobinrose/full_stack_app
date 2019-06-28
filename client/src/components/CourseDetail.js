159399104a8c09e28d8e2d685d7e3f4e4ff8fd9f
Mary Robin Rose <maryrobinrose@gmail.com>
2019-06-28 16:12:53 -0700

    add initial component

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -3,3 +3,21 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import ReactMarkdown from 'react-markdown';
 import axios from 'axios';
+
+class CourseDetail extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        course: [],
+        user: []
+      };
+      this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  componentDidMount() {
+    
+  }
+}
+
+
+export default CourseDetail;
