fceb54d8f88700b047c67b2e9e6c141c96f9e1e4
Mary Robin Rose <maryrobinrose@gmail.com>
2019-06-29 14:25:46 -0700

    add state

--- client/src/components/UserSignUp.js
+++ client/src/components/UserSignUp.js
@@ -1,3 +1,25 @@
 //Import React library
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+
+class UserSignUp extends Component {
+  state = {
+    firstName: '',
+    lastName: '',
+    emailAddress: '',
+    password: '',
+    confirmPassword: '',
+    validationErrors: ''
+  };
+
+
+
+
+
+
+
+  
+}
+
+
+export default UserSignUp;
