b12eb4e3f32d045e541f72ceb17bd3d36a96aca4
Mary Robin Rose <maryrobinrose@gmail.com>
2019-06-29 13:42:06 -0700

    add links and buttons

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -51,11 +51,15 @@ class CourseDetail extends Component {
     })
   }
 
+
   render() {
     return(
       <div className='actions--bar'>
         <div className='bounds'>
           <div className='grid-100'>
+            <Link className='button button-secondary' to='/courses'>Return to List</Link>
+            <Link className='button' to={'/courses'+this.state.course.id+'/update'}>Update Course</Link>
+            <button className='button' onClick={e => this.handleDeleteCourse()}>Delete Course</button>
           </div>
         </div>
       </div>
