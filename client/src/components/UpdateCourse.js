b646536a2c11bf5ebea72a7e8c5348bac76cff35
Mary Robin Rose <maryrobinrose@gmail.com>
2019-06-30 14:10:36 -0700

    add state

--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -5,6 +5,17 @@ import axios from 'axios';
 
 class UpdateCourse extends Component {
 
+  state = {
+    id: '',
+    title: '',
+    description: '',
+    estimatedTime: '',
+    materialdsNeeded: '',
+    userid: '',
+    createdBy: '',
+    validationErrors: ''
+  }
+
 }
 
 export default UpdateCourse;
