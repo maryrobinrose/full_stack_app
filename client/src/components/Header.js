d62f241152869c1e1aaeaef23474a6f6fa7b0793
Mary Robin Rose <maryrobinrose@gmail.com>
2019-06-29 17:46:48 -0700

    add nav links

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,19 +1,19 @@
-//This is from project 7
-
-
 //Connect to React and routes
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header className="main-nav">
-      <ul>
-        <li><NavLink exact to="/sunsets">Sunsets</NavLink></li>
-        <li><NavLink to="/waterfalls">Waterfalls</NavLink></li>
-        <li><NavLink to="/rainbows">Rainbows</NavLink></li>
-      </ul>
-    </header>
+      <div className='header'>
+          <div className='bounds header-bounds'>
+            <h1 clasName='header--logo'>Courses</h1>
+            <nav>
+            <Link className='signin' to={'/UserSignIn'}>Sign In</Link>
+            <Link className='signup' to={'/UserSignUp'}>Sign Up</Link>
+            </nav>
+          </div>
+      </div>
+
   );
 }
 
