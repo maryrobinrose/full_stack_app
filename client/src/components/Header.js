aca40125c7bb155e7d4b43a15bace8774dfb7dc6
Mary Robin Rose <maryrobinrose@gmail.com>
2019-06-29 18:28:00 -0700

    add conditionals

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,6 +4,19 @@ import { NavLink, Link } from 'react-router-dom';
 
 const Header = () => {
   return (
+
+    //If user is logged in
+    if (localStorage.getItem()) {
+      <div className='header'>
+          <div className='bounds header-bounds'>
+            <NavLink to='/' className='header--logo'>Courses</NavLink>
+            <nav>
+              <span>Welcome {localStorage.getItem('name')}</span>
+            </nav>
+      </div>
+
+      //If user is not logged in
+    } else {
       <div className='header'>
           <div className='bounds header-bounds'>
             <NavLink to='/' className='header--logo'>Courses</NavLink>
@@ -13,6 +26,8 @@ const Header = () => {
             </nav>
           </div>
       </div>
+    }
+
 
   );
 }
