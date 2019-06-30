08b232efa6da4b1f1c34d8626f2b28cf007d4dc3
Mary Robin Rose <maryrobinrose@gmail.com>
2019-06-30 14:06:27 -0700

    add initial code

---
+++ client/src/components/UpdateCourse.js
@@ -0,0 +1,10 @@
+//Import React library
+import React, { Component } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import axios from 'axios';
+
+class UpdateCourse extends Component {
+
+}
+
+export default UpdateCourse;
