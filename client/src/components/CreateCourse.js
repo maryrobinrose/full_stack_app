88cde45fadf571f0ff9f8b42ef835a01714755bd
Mary Robin Rose <maryrobinrose@gmail.com>
2019-06-30 12:57:27 -0700

    add initial code

---
+++ client/src/components/CreateCourse.js
@@ -0,0 +1,11 @@
+//Import React library
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+class CreateCourse extends Component {
+
+
+}
+
+export default CreateCourse;
