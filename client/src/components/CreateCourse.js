7c781f6f8c9f0a62e3bce7150b2c57e909fbe249
Mary Robin Rose <maryrobinrose@gmail.com>
2019-06-30 13:01:04 -0700

    add on change

--- client/src/components/CreateCourse.js
+++ client/src/components/CreateCourse.js
@@ -5,6 +5,24 @@ import axios from 'axios';
 
 class CreateCourse extends Component {
 
+  state = {
+    title: '',
+    description: '',
+    estimatedTime: '',
+    materialdsNeeded: '',
+    validationErrors: '',
+  }
+
+  //Handle user changes
+  onChange = e => {
+    this.setState({[e.target.name]: e.target.value});
+  }
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    axios({})
+  }
 
 }
 
