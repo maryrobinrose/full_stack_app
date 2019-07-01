476bf56e7942fd18800333af86680cccab27a8d6
Mary Robin Rose <maryrobinrose@gmail.com>
2019-07-01 12:38:43 -0700

    add text

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,4 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+const NotFound = () => {
+  return(
+    <div>
+      <div className='bounds'>
+        <h1>Not Found</h1>
+          <p>This page could not be found.</p>
+      </div>
+    </div>
+  )
+}
+
 export default NotFound;
