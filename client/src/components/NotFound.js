57bcb2f82ad038cf45a981c252678997a996e492
Mary Robin Rose <maryrobinrose@gmail.com>
2019-07-01 12:39:43 -0700

    add return to list link

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -7,6 +7,7 @@ const NotFound = () => {
       <div className='bounds'>
         <h1>Not Found</h1>
           <p>This page could not be found.</p>
+          <Link className='button button-secondary' to='/courses'>Return to List</Link>
       </div>
     </div>
   )
