0ea27ad5882ccfd37e98de32e2d07f62fb9b12ca
Mary Robin Rose <maryrobinrose@gmail.com>
2019-06-30 18:32:26 -0700

    add function

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -2,12 +2,25 @@
 import React, { Component } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-const PrivateRoute = () => {
-  <Route
-    {...rest}
-    
-
-  />
+//https://reacttraining.com/react-router/web/example/auth-workflow
+function PrivateRoute({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        localStorage.getItem('username') ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/signin",
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
+  );
 }
 
 
