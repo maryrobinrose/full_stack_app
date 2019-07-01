4de5ff02b90cbdb59bb602004f8c51cb85a1b56a
Mary Robin Rose <maryrobinrose@gmail.com>
2019-06-30 18:14:31 -0700

    add private route

---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,14 @@
+//Import React library
+import React, { Component } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const PrivateRoute = () => {
+  <Route
+    {...rest}
+    
+
+  />
+}
+
+
+export default PrivateRoute;
