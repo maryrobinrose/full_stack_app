6ebfb935d6ddf29d12fe8b1b83bc0cf71014e9b7
Mary Robin Rose <maryrobinrose@gmail.com>
2019-07-01 16:43:33 -0700

    match markup

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -6,8 +6,7 @@ const NotFound = () => {
     <div>
       <div className='bounds'>
         <h1>Not Found</h1>
-          <p>This page could not be found.</p>
-          <Link className='button button-secondary' to='/courses'>Return to List</Link>
+          <p>Sorry! We couldn't find the page you're looking for.</p>
       </div>
     </div>
   )
