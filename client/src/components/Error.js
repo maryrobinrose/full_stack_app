0c794c85874eadc3f1813bb7b8887410e4fb7c17
Mary Robin Rose <maryrobinrose@gmail.com>
2019-07-01 12:24:55 -0700

    add error return

--- client/src/components/Error.js
+++ client/src/components/Error.js
@@ -1,4 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+const Error = (error) => {
+  return(
+    <div id='root'>
+      <div className='bounds'>
+        <h1>Error</h1>
+          <p>An unexpected error has occured.</p>
+          <Link className='button button-secondary' to='/courses'>Return to List</Link>
+      </div>
+    </div>
+  )
+}
+
 export default Error;
