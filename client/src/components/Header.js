d7d2aba2538b8513c4b2037244512bd13bfc78c1
Mary Robin Rose <maryrobinrose@gmail.com>
2019-07-01 15:39:08 -0700

    remove comments

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -28,39 +28,8 @@ const Header = () => {
         </div>
       )
     )}
-
     </UserContext.Consumer>
   )
-
-  //If user is signed in
-  {/*if(JSON.parse(localStorage.getItem('IsLoggedIn'))) {
-    return(
-      <div className='header'>
-          <div className='bounds header-bounds'>
-            <NavLink to='/' className='header--logo'>Courses</NavLink>
-            <nav>
-              <span>Welcome {localStorage.getItem('name')}!</span>
-              <Link to='/signout' className='signout'>Sign Out</Link>
-            </nav>
-          </div>
-      </div>
-    )
-
-    //If user is not logged in
-  } else {
-    return(
-      <div className='header'>
-          <div className='bounds header-bounds'>
-            <NavLink to='/' className='header--logo'>Courses</NavLink>
-            <nav>
-            <Link className='signin' to={'/UserSignIn'}>Sign In</Link>
-            <Link className='signup' to={'/UserSignUp'}>Sign Up</Link>
-            </nav>
-          </div>
-      </div>
-    )
-  }*/}
-
 }
 
 
