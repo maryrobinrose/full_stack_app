140eab8212ce51daa6668afea37999faf6903775
Mary Robin Rose <maryrobinrose@gmail.com>
2019-07-01 17:17:58 -0700

    add user context

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -1,6 +1,7 @@
 //Import React library
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import UserContext from './UserContext';
 
 class UserSignIn extends Component {
 
@@ -19,36 +20,39 @@ class UserSignIn extends Component {
 
   render(){
     return(
-      <div className='bounds'>
-        <div className='grid-33 centered signin'>
-          <h1>Sign In</h1>
-          <div>
-            <form onSubmit={this.handleSubmit}>
-              <div><input
-                id='emaillAddress'
-                name = 'emailAddress'
-                type='text'
-                className=''
-                placeholder='Email Address' value={this.state.emailAddress}
-                onChange={e => this.change(e)}
-              /></div>
-              <div><input
-                id='password'
-                name = 'password'
-                type='password'
-                className=''
-                placeholder='Password' value={this.state.password}
-                onChange={e => this.change(e)}
-              /></div>
-              <div className="grid-100 pad-bottom">
-                <button className="button" type="submit">Sign In</button>
-                <Link to='/' className="button button-secondary">Cancel</Link>
-              </div>
-            </form>
+      <UserContext.Consumer>
+      {/*Add Authentication*/}
+        <div className='bounds'>
+          <div className='grid-33 centered signin'>
+            <h1>Sign In</h1>
+            <div>
+              <form onSubmit={this.handleSubmit}>
+                <div><input
+                  id='emaillAddress'
+                  name = 'emailAddress'
+                  type='text'
+                  className=''
+                  placeholder='Email Address' value={this.state.emailAddress}
+                  onChange={e => this.change(e)}
+                /></div>
+                <div><input
+                  id='password'
+                  name = 'password'
+                  type='password'
+                  className=''
+                  placeholder='Password' value={this.state.password}
+                  onChange={e => this.change(e)}
+                /></div>
+                <div className="grid-100 pad-bottom">
+                  <button className="button" type="submit">Sign In</button>
+                  <Link to='/' className="button button-secondary">Cancel</Link>
+                </div>
+              </form>
+            </div>
+            <p>Don't have a user account? <Link to='/signup'>Click here</Link> to sign up!</p>
           </div>
-          <p>Don't have a user account? <Link to='/signup'>Click here</Link> to sign up!</p>
         </div>
-      </div>
+      </UserContext.Consumer>
     )
   }
 
