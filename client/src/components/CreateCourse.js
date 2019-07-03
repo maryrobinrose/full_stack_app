3ca417cb458c736bf604ccdcc6d0d521614fd329
Mary Robin Rose <maryrobinrose@gmail.com>
2019-07-02 18:59:38 -0700

    change names to handle

--- client/src/components/CreateCourse.js
+++ client/src/components/CreateCourse.js
@@ -14,12 +14,12 @@ class CreateCourse extends Component {
   }
 
   //Handle user changes
-  onChange = e => {
+  handleChange = e => {
     this.setState({[e.target.name]: e.target.value});
   }
 
   //Handle created course
-  onSubmit = e => {
+  handleCreate = e => {
     e.preventDefault();
 
     const {title, description} = this.state;
@@ -78,69 +78,73 @@ class CreateCourse extends Component {
             </div>
           </div>
         ) : ''}
+
+          <form onSubmit={e => this.onSubmit(e, localStorage.getItem('username'), localStorage.getItem('password'))}>
+            <div className='grid-66'>
+              <div className='course--header'>
+                <h4 className='course--label'>Course</h4>
+                <div>
+                  <input
+                    id='title'
+                    name='title'
+                    type='text'
+                    className='input-title course--title--input'
+                    placeholder='Course title...'
+                    onChange={this.onSubmit}
+                  />
+                </div>
+                <p>By {this.state.username}</p>
+              </div>
+                <div className='course--description'>
+                  <textarea
+                    id='description'
+                    name='description'
+                    className=''
+                    placeholder='Course Description...'
+                    onChange={this.onSubmit} />
+                </div>
+            </div>
+            <div className='grid-25 grid-right'>
+              <div className='course--stats'>
+                <ul className='course--stats--list'>
+                  <li className='class--stats--list--item'>
+                    <h4>Estimated Time</h4>
+                    <div>
+                      <input
+                        value={this.state.estimatedTime}
+                        onChange={e => this.change(e)}
+                        id='estimatedTime'
+                        name='estimatedTime'
+                        type='text'
+                        className='course--time--input'
+                        placeholder='Hours'
+                      />
+                    </div>
+                  </li>
+                  <li className='course--stats--list--item'>
+                    <h4>Materials Needed</h4>
+                    <div>
+                      <textarea
+                        value={this.state.materialdsNeeded}
+                        onChange={e => this.change(e)}
+                        id='materialdsNeeded'
+                        name='materialdsNeeded'
+                        placeholder='List materials...'
+                      />
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className='grid-100 pad-bottom'>
+              {/*Create Course button*/}
+              <button className='button' type='submit'>Create Course</button>
+
+              {/*Cancel button*/}
+              <NavLink to='/' className='button button-secondary'>Cancel</NavLink>
+            </div>
+          </form>
         </div>
-        <form onSubmit={e => this.onSubmit(e, localStorage.getItem('username'), localStorage.getItem('password'))}>
-          <div className='grid-66'>
-            <div className='course--header'>
-              <h4 className='course--label'>Course</h4>
-              <div>
-                <input
-                  id='title'
-                  name='title'
-                  type='text'
-                  className='input-title course--title--input'
-                  placeholder='Course title...'
-                  onChange={this.onSubmit}
-                />
-              </div>
-              <p>By{localStorage.getItem('name')}</p>
-            </div>
-              <div className='course--description'>
-                <textarea
-                  id='description'
-                  name='description'
-                  className=''
-                  placeholder='Course Description...'
-                  onChange={this.onSubmit} />
-              </div>
-          </div>
-          <div className='grid=25 grid-right'>
-            <div className='course--stats'>
-              <ul className='course--stats--list'>
-                <li className='class--stats--list--item'>
-                  <h4>Estimated Time</h4>
-                  <div>
-                    <input
-                      value={this.state.estimatedTime}
-                      onChange={e => this.change(e)}
-                      id='estimatedTime'
-                      name='estimatedTime'
-                      type='text'
-                      className='course--time--input'
-                      placeholder='Hours'
-                    />
-                  </div>
-                </li>
-                <li className='course--stats--list--item'>
-                  <h4>Materials Needed</h4>
-                  <div>
-                    <textarea
-                      value={this.state.materialdsNeeded}
-                      onChange={e => this.change(e)}
-                      id='materialdsNeeded'
-                      name='materialdsNeeded'
-                      placeholder='List materials...'
-                    />
-                  </div>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className='grid-100 pad-bottom'>
-            <button className='button' type='submit'>Create Course</button>
-            <NavLink to='/' className='button button-secondary'>Cancel</NavLink>
-          </div>
-        </form>
       </div>
     )
   }
