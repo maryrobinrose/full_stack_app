9b9571d2831901a9dc4268692bd4441581d387d0
Mary Robin Rose <maryrobinrose@gmail.com>
2019-07-04 13:51:16 -0700

    work on conditionals

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,30 +7,48 @@ const Header = () => {
   return (
     <UserContext.Consumer>{ ({authenticated, signOut, user}) => (
 
+      localStorage.getItem('username') ? (
+
 
         <div className='header'>
           <div className='bounds'>
+
+            {/*Link back to main Courses*/}
             <h1 className='header--logo'>
               <NavLink to='/courses' className='header--logo'>Courses</NavLink>
             </h1>
-            {(authenticated) ?
-            (
+
               <nav>
+                {/*Wecomes user by name*/}
                 <span>Welcome {localStorage.getItem('name')}!</span>
+
+                {/*Link for Signing Out*/}
                 <Link to='/signout' onClick= {signOut} className='signout'>Sign Out</Link>
               </nav>
+
+          </div>
+        </div>
 
           )
           :
           (
+
+            <div className='header'>
+              <div className='bounds'>
+
+              {/*Link back to main Courses*/}
+              <h1 className='header--logo'>
+                <NavLink to='/courses' className='header--logo'>Courses</NavLink>
+              </h1>
+
               <nav>
-              <Link className='signin' to={'/signin'}>Sign In</Link>
-              <Link className='signup' to={'/signup'}>Sign Up</Link>
+                <Link className='signin' to={'/signin'}>Sign In</Link>
+                <Link className='signup' to={'/signup'}>Sign Up</Link>
               </nav>
-          )
-        }
+              
+              </div>
             </div>
-        </div>
+          )
     )}
     </UserContext.Consumer>
   )
