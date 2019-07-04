ec1a3e03d00d473d89efadd9deab45bd4c86d600
Mary Robin Rose <maryrobinrose@gmail.com>
2019-07-03 18:03:22 -0700

    add comments

--- client/src/App.js
+++ client/src/App.js
@@ -101,7 +101,10 @@ class App extends Component {
 
   render() {
     return (
+
+      {/*Allows consuming components to subscribe to context changes*/}
       <UserContext.Provider
+      {/* render based on the context value */}
         value={{
           user: this.state.user,
           emailAddress: this.state.emailAddress,
