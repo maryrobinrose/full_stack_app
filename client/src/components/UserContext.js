ef823fa77e3f0de2c640160887548b7697fc0e32
Mary Robin Rose <maryrobinrose@gmail.com>
2019-07-05 15:16:55 -0700

    work on context

--- client/src/components/UserContext.js
+++ client/src/components/UserContext.js
@@ -1,10 +1,20 @@
-import React from 'react';
+/*This was created with help from:
+https://www.taniarascia.com/using-context-api-in-react/
+and Treehouses's React Context API Course
+*/
+
+//**Provides a way to pass data to components without having to pass props manually at every level**//
+
+import React { Component } from 'react';
+import axios from 'axios';
 
 //React Context API
 const UserContext = React.createContext();
 
-//https://www.taniarascia.com/using-context-api-in-react/
+
 export const Provider = UserContext.Provider
+
+
 export const Consumer = UserContext.Consumer
 
 export default UserContext;
