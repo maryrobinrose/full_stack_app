034caa8b947ed6d19866a97a6f6fe6f165f752a9
Mary Robin Rose <maryrobinrose@gmail.com>
2019-07-05 22:30:57 -0700

    working on render

--- client/src/components/CreateCourse.js
+++ client/src/components/CreateCourse.js
@@ -28,151 +28,139 @@ class CreateCourse extends Component {
   time = React.createRef();
   materials = React.createRef();
 
-  //Handle user changes
-  handleChange = e => {
-    this.setState({[e.target.name]: e.target.value});
-  }
-
-  //Handle created course
-  handleCreate = e => {
-    e.preventDefault();
-
-    const {title, description, estimatedTime, materialsNeeded} = this.state;
-
-    if(title === '') {
-      this.setState({
-        showError: 'Please enter a title.'
-      })
-    } else if (description === '') {
-      this.setState({
-        showError: 'Please enter a description.'
-      })
-    /*} else if (description === '' && title === '') {
-      this.setState({
-        showError: 'Please enter a description and a title.'
-      })*/
-    } else {
-      axios({
-        method: 'post',
-        url: 'http://localhost:5000/api/courses',
-        auth: {
-          username: localStorage.getItem('username'),
-          password: localStorage.getItem('password')
-        },
-        data: {
-          user: localStorage.getItem('id'),
-          title,
-          description,
-          estimatedTime,
-          materialsNeeded
-        }
-      })
-      //.then(alert('Your course has been created'))
-      .then(() => {
-        //Reset inputs
-        this.setState({
-          id: '',
-          title: '',
-          description: '',
-          estimatedTime: '',
-          materialsNeeded: '',
-          showError: ''
-        });
-        //Send user back to main
-        this.props.history.push('/courses');
-      })
-      .catch(error => {
-        this.setState({
-          errors: error.response.data.errors
-        })
-      })
-    }
-  }
 
   render() {
 
     return(
       <Consumer>
-        <div className='bounds course--detail'>
-          <h1>Create Course</h1>
-          <div>
-          {showError ? (
-            <div>
-              <h2 className="validation--errors--label">Validation errors</h2>
-              <div className="validation-errors">
-                <ul>
-                  <li>{showError}</li>
-                </ul>
-              </div>
-            </div>
-          ) : ''}
+        {({ username, password, name, userId}) => {
 
-            <form onSubmit={e => this.handleCreate(e, localStorage.getItem('username'), localStorage.getItem('password'), title, description, estimatedTime, materialsNeeded)}>
-              <div className='grid-66'>
-                <div className='course--header'>
-                  <h4 className='course--label'>Course</h4>
-                  <div>
-                    <input
-                      id='title'
-                      name='title'
-                      type='text'
-                      className='input-title course--title--input'
-                      placeholder='Course title...'
-                      onChange={this.handleChange}
-                    />
-                  </div>
-                  <p>By {this.state.username}</p>
-                </div>
-                  <div className='course--description'>
-                    <textarea
-                      id='description'
-                      name='description'
-                      className=''
-                      placeholder='Course Description...'
-                      onChange={this.handleChange} />
-                  </div>
-              </div>
-              <div className='grid-25 grid-right'>
-                <div className='course--stats'>
-                  <ul className='course--stats--list'>
-                    <li className='class--stats--list--item'>
-                      <h4>Estimated Time</h4>
-                      <div>
+          //Handle user changes
+          const handleCreate = e => {
+            e.preventDefault();
+            this.setState({
+              errors: []
+            });
+          }
+
+
+          // Use POST method to send new info
+          axios({
+            method: 'POST',
+            url: 'http://localhost:5000/api/courses',
+            auth: {
+              username: username,
+              password: password
+            },
+            responseType: 'json',
+            data: {
+              title: this.title.current.value,
+              description: this.description.current.value,
+              estimatedTime: this.time.current.value,
+              materialsNeeded: this.materials.current.value,
+              userId: userId
+            }
+          })
+          .then(res => {
+            this.props.history.push(`/courses/${res.data.id}`);
+          })
+          .catch(error => {
+            if (error.response.status === 401) {
+              this.props.history.push('/signin');
+            }
+            if (error.response.status === 400) {
+              let errors = error.response.data.errors;
+              let errorMessage = erros.map(
+                (error, index) => (
+                  <li key={index}>{error}</li>
+                )
+              );
+              this.setState({
+                errors: errorMessage
+              });
+            }
+          });
+        }
+      }
+
+
+        return (
+
+          <div className='bounds course--detail'>
+
+              <form onSubmit={handleCreate}>
+                <h1>Create Course</h1>
+                  <ul>{this.state.errors}</ul>
+
+                  <div className='grid-66'>
+                    <div className='course--header'>
+                      <h4 className='course--label'>Course</h4>
+
                         <input
-                          id='estimatedTime'
-                          name='estimatedTime'
-                          className='course--time--input'
+                          id='title'
+                          name='title'
                           type='text'
-                          placeholder='Hours'
-                          onChange={this.handleChange} />
-                      </div>
-                    </li>
-                    <li className='course--stats--list--item'>
-                      <h4>Materials Needed</h4>
-                      <div>
-                        <textarea
-                          id='materialsNeeded'
-                          name='materialsNeeded'
-                          className=''
-                          placeholder='List materials...'
-                          onChange={this.handleChange} />
-                      </div>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <div className='grid-100 pad-bottom'>
-                {/*Create Course button*/}
-                <button className='button' type='submit'>Create Course</button>
+                          className='input-title course--title--input'
+                          placeholder='Course title...'
+                          ref={this.title}
+                        />
 
-                {/*Cancel button*/}
-                <NavLink to='/courses' className='button button-secondary'>Cancel</NavLink>
-              </div>
-            </form>
+                      <p>By {name}</p>
+                      <div className='course--description'>
+                        <textarea
+                          id='description'
+                          name='description'
+                          placeholder='Course Description...'
+                          ref={this.description}
+                          />
+                      </div>
+
+
+                <div className='grid-25 grid-right'>
+                  <div className='course--stats'>
+                    <ul className='course--stats--list'>
+                      <li className='class--stats--list--item'>
+                        <h4>Estimated Time</h4>
+
+                          <input
+                            id='estimatedTime'
+                            name='estimatedTime'
+                            className='course--time--input'
+                            type='text'
+                            placeholder='Hours'
+                            ref={this.time}
+                            />
+                      </li>
+
+                      <li className='course--stats--list--item'>
+                        <h4>Materials Needed</h4>
+
+                          <textarea
+                            id='materialsNeeded'
+                            name='materialsNeeded'
+                            placeholder='List materials...'
+                            ref={this.materials}
+                            />
+                      </li>
+
+                    </ul>
+                  </div>
+                </div>
+
+                <div className='grid-100 pad-bottom'>
+                  {/*Create Course button*/}
+                  <button className='button' type='submit'>Create Course</button>
+
+                  {/*Cancel button*/}
+                  <NavLink to='/courses' className='button button-secondary'>Cancel</NavLink>
+                </div>
+              </form>
+              
+            </div>
           </div>
-        </div>
-      </Consumer>
-    )
+        }}
+        </Consumer>
+      );
   }
 }
 
