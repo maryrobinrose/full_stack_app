45bfdc6c67655f57a2d540b08a1d3edc075c8cf0
Mary Robin Rose <maryrobinrose@gmail.com>
2019-07-05 21:40:20 -0700

    add comments

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -1,3 +1,5 @@
+//**This component provides the "Courses" screen by retrieving the list of courses from the REST API's /api/courses route and rendering a list of courses. Each course needs to link to its respective "Course Detail" screen. This component also renders a link to the "Create Course" screen.**//
+
 //Imports
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
@@ -16,7 +18,9 @@ class Courses extends Component {
     componentDidMount() {
       //Request the API
       axios.get('http://localhost:5000/api/courses/')
+      //Grab json
       .then(res => res.json())
+
       .then (resData => {
         this.setState({
           courses: resData.courses
@@ -25,14 +29,14 @@ class Courses extends Component {
       //Log errors
       .catch(error => {
         console.log('Error fetching and parsing data', error);
+        //this.props.history.push('/error');
       })
     }
 
     render() {
 
-        let showCourses;
-
-        showCourses = this.state.courses.map(course => (
+        //Map over courses
+        let showCourses = this.state.courses.map(course => (
           <CourseBox
             id={course.id}
             key={course.id}
@@ -46,6 +50,7 @@ class Courses extends Component {
 
               {showCourses}
 
+              {/*Show Add New Course button*/}
               <li>
                 <Link className='course--module course--add--module' to='/courses/create'>
                   <h3 className="course--add--title"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
