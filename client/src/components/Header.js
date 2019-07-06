56ffed99b1e7a7d6a07c5f74b4fa3eec5c694af6
Mary Robin Rose <maryrobinrose@gmail.com>
2019-07-05 18:21:28 -0700

    working on header conditionals

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,58 +1,52 @@
-//Connect to React and routes
+//**Displays the top menu bar for the application and includes buttons for signing in and signing up (if there's not an authenticated user) or the user's first and last name and a button for signing out (if there's an authenticated user).**//
+
+
+//Imports
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
-import { Provider } from './components/Context';
+import { Consumer } from './components/Context';
 
 const Header = () => {
   return (
-    <Consumer>{ ({signOut}) => (
+    <Consumer>{ ({signedIn, name}) => {
 
-      localStorage.getItem('username') ? (
-
-
-        <div className='header'>
-          <div className='bounds'>
-
-            {/*Link back to main Courses*/}
-            <h1 className='header--logo'>
-              <NavLink to='/courses' className='header--logo'>Courses</NavLink>
-            </h1>
-
-              <nav>
-                {/*Wecomes user by name*/}
-                <span>Welcome {localStorage.getItem('name')}!</span>
-
-                {/*Link for Signing Out*/}
-                <Link to='/signout' onClick= {signOut} className='signout'>Sign Out</Link>
-              </nav>
-
-          </div>
-        </div>
-
-        )
-        :
-        (
-
-        <div className='header'>
-          <div className='bounds'>
+      return(
+        <header>
+          <div className='header'>
+            <div className='bounds'>
 
               {/*Link back to main Courses*/}
               <h1 className='header--logo'>
                 <NavLink to='/courses' className='header--logo'>Courses</NavLink>
               </h1>
 
-              <nav>
-                {/*Link to Sign In Page*/}
-                <Link className='signin' to={'/signin'}>Sign In</Link>
-                {/*Link to Sign Out Page*/}
-                <Link className='signup' to={'/signup'}>Sign Up</Link>
-              </nav>
+              <nav className='main-nav'>
+                {/*Wecomes user by name*/}
 
+
+                {/*Link for Signing Out*/}
+
+
+
+
+
+                {/*Link back to main Courses*/}
+                <h1 className='header--logo'>
+                  <NavLink to='/courses' className='header--logo'>Courses</NavLink>
+                </h1>
+
+
+                  {/*Link to Sign In Page*/}
+                  <Link className='signin' to={'/signin'}>Sign In</Link>
+                  {/*Link to Sign Out Page*/}
+                  <Link className='signup' to={'/signup'}>Sign Up</Link>
+
+
+            </nav>
           </div>
-        </div>
-
-        )
-    )}
+        </header>
+      )
+    }}
     </Consumer>
   )
 }
