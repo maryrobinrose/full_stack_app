bafe3e5f5843a791fad0f7e607f5c982c18aa603
Mary Robin Rose <maryrobinrose@gmail.com>
2019-07-05 20:55:26 -0700

    work on course

--- client/src/components/Course.js
+++ client/src/components/Course.js
@@ -1,17 +1,17 @@
 //**Provides holder for each course**//
 
 //Imports
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Course = ({ id, title}) => {
   return(
-    <li>
-      <Link to={`/courses/${id}`}>
-        <div>Course</div>
-        <div>{title}</div>
-      <Link>
-    </li>
+    <li key={id}>
+     <Link to={`/courses/${id}`}>
+       <div>Course</div>
+       <div>{title}</div>
+     </Link>
+   </li>
   );
 }
 
--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -23,7 +23,6 @@ class Courses extends Component {
       //Log errors
       .catch(error => {
         console.log('Error fetching and parsing data', error);
-        this.props.history.push('/error');
       })
     }
 
@@ -40,21 +39,21 @@ class Courses extends Component {
         ));
 
         return(
-
-
-          {/*Link to Create Course*/}
-        <div className='grid-33'>
-          <Link className='course--module course--add--module' to='/courses/create'>
-          <h3 className="course--add--title"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-              viewBox="0 0 13 13" className="add">
-              <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
-            </svg>New Course</h3>
-          </Link>
-        </div>
-
+          <div>
+            <ul>
+              {showCourses}
+              <li>
+                <Link className='course--module course--add--module' to='/courses/create'>
+                <h3 className="course--add--title"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                    viewBox="0 0 13 13" className="add">
+                    <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
+                  </svg>New Course</h3>
+                </Link>
+              </li>
+            </ul>
+          </div>
       );
     }
 }
-
 
   export default Courses;
