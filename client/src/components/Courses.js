314341d36b4767635dca84e37992f5b09c85c415
Mary Robin Rose <maryrobinrose@gmail.com>
2019-07-09 06:26:57 -0700

    add grid and bounds divs

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -37,30 +37,32 @@ class Courses extends Component {
     render() {
 
         return(
-          <div>
-            <ul>
+          <div className="bounds">
+            <div className="grid-33">
+              <ul>
 
-            {/*Map over courses*/}
-            {this.state.courses.map(course => (
-              <CourseBox
-                id={course.id}
-                key={course.id}
-                title={course.title}
-              />
-            ))}
+              {/*Map over courses*/}
+              {this.state.courses.map(course => (
+                <CourseBox
+                  id={course.id}
+                  key={course.id}
+                  title={course.title}
+                />
+              ))}
 
 
-              {/*Show Add New Course button*/}
-              <li>
-                <Link className='course--module course--add--module' to='/courses/create'>
-                  <h3 className="course--add--title"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-                      viewBox="0 0 13 13" className="add">
-                      <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
-                    </svg>New Course</h3>
-                </Link>
+                {/*Show Add New Course button*/}
+                <li>
+                  <Link className='course--module course--add--module' to='/courses/create'>
+                    <h3 className="course--add--title"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                        viewBox="0 0 13 13" className="add">
+                        <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
+                      </svg>New Course</h3>
+                  </Link>
 
-              </li>
-            </ul>
+                </li>
+              </ul>
+            </div>
           </div>
       );
     }
