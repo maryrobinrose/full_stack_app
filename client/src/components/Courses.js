dd590d9f3ba1d884c931219dc9507d78a5ed66b4
Mary Robin Rose <maryrobinrose@gmail.com>
2019-07-09 06:31:14 -0700

    add grid 33 div to new course

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -38,7 +38,7 @@ class Courses extends Component {
 
         return(
           <div className="bounds">
-            <div className="grid-33">
+
               <ul>
 
               {/*Map over courses*/}
@@ -52,17 +52,19 @@ class Courses extends Component {
 
 
                 {/*Show Add New Course button*/}
-                <li>
-                  <Link className='course--module course--add--module' to='/courses/create'>
-                    <h3 className="course--add--title"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-                        viewBox="0 0 13 13" className="add">
-                        <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
-                      </svg>New Course</h3>
-                  </Link>
+                <div className="grid-33">
+                  <li>
+                    <Link className='course--module course--add--module' to='/courses/create'>
+                      <h3 className="course--add--title"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                          viewBox="0 0 13 13" className="add">
+                          <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
+                        </svg>New Course</h3>
+                    </Link>
 
-                </li>
+                  </li>
+                </div>
               </ul>
-            </div>
+
           </div>
       );
     }
